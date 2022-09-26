07d5371a9ecbc637638d69f08f6e6dc6b25ed2fc
Egor Ivantsov <E.Ivantsov@mobilepark.ru>
2022-09-27 00:56:12 +0300

    Functional component

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+function ShopItemFunc({item}) {
+  return (
+      <div className="main-content">
+          <h2>
+              {item.brand}
+          </h2>
+
+          <h1>
+              {item.title}
+          </h1>
+          
+          <h3>
+              {item.description}
+          </h3>
+
+          <div className="description">
+              {item.descriptionFull}
+          </div>
+
+          <div className="highlight-window mobile">
+              <div className="highlight-overlay" />
+          </div>
+
+          <div className="divider" />
+
+          <div className="purchase-info">
+              <div className="price">
+                  {item.currency + item.price.toFixed(2)}
+              </div>
+
+              <button type="button">
+                  Добавить в корзину
+              </button>
+          </div>
+      </div>
+  );
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.object.isRequired
+}
+
+export default ShopItemFunc;
