ec137eba9c6384619e4589c4936dfd475c8c9a5b
Egor Ivantsov <E.Ivantsov@mobilepark.ru>
2022-09-30 21:16:43 +0300

    Functional components are ready.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './css/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
